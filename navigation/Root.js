d7c0c6005bdfc85cecded8168406352ea30a5031
YEACHAN <everever1275@gmail.com>
2022-12-30 20:14:38 +0900

    221230 done

---
+++ navigation/Root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Tabs from "./Tabs";
+import Stack from "./Stack";
+
+const Nav = createStackNavigator();
+
+const Root = () => (
+    <Nav.Navigator screenOptions={{
+      headerShown: false,
+    }}>
+      <Nav.Screen name="Tabs" component={Tabs} />
+      <Nav.Screen name="Stack" component={Stack} />
+  </Nav.Navigator>
+);
+
+export default Root;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,18 +3,60 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tv from "../screens/Television";
 import Search from "../screens/Search";
 import Movies from "../screens/Movie";
-import {Text, View} from "react-native";
-import {StyleSheet} from "react-native";
+import {useColorScheme} from 'react-native';
+import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
+import Stack from "./Stack";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  return(
     <Tab.Navigator screenOptions={{
-      tabBarActiveTintColor: "coral",
+      tabBarActiveTintColor: isDark ? "coral" : colors.gray,
+      tabBarStyle: {
+        backgroundColor: isDark ? colors.dark_blue : colors.sky_blue
+      },
+      tabBarInactiveTintColor: isDark ? "white" : colors.black,
+      headerStyle : {
+        backgroundColor: isDark ? colors.dark_blue : colors.sky_blue,
+      },
+      headerTitleStyle : {
+        color : isDark ? "white" : "black"
+      },
+      tabBarLabelStyle: {
+        fontSize: 13,
+        fontWeight: "500",
+        marginTop : "-5%",
+      },
     }}>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon : ({color, size}) => (
+            <Ionicons name="film" color={color} size={size} />
+         )
+      }} />
+      <Tab.Screen
+          name="TV"
+          component={Tv}
+          options={{
+            tabBarIcon : ({color, size}) => (
+                <Ionicons name="tv" color={color} size={size} />
+            )
+          }} />
+      <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarIcon : ({focused, color, size}) => {
+              console.log(focused, color, size)
+              return <Ionicons name={focused ? "search" : "search-circle"} color={color} size={size} />
+          }
+          }}
+      />
     </Tab.Navigator>
-);
+)};
 export default Tabs;
