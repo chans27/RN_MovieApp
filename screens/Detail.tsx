ea2783f02396c72210ac447d20c4bd7996e670a3
YEACHAN <everever1275@gmail.com>
2023-01-05 20:46:29 +0900

    3.20

---
+++ screens/Detail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components/native";
+import {Text, View} from "react-native";
+
+const Detail = () => (
+  <View>
+    <Text>Detail</Text>
+  </View>
+);
+
+export default Detail;
